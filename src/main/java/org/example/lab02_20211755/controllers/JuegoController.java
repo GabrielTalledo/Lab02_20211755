51b48600012395d4fec95de7f5ef62d91a8734c9
GabrielTalledo <a20211755@pucp.edu.pe>
2024-04-11 21:55:42 -0500

    feat: Arreglos de la vista.

--- src/main/java/org/example/lab02_20211755/controllers/JuegoController.java
+++ src/main/java/org/example/lab02_20211755/controllers/JuegoController.java
@@ -9,9 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-@RequestMapping(value = "/jugar")
 public class JuegoController {
 
     final AbiertoRepository abiertoRepository;
@@ -25,13 +27,31 @@ public class JuegoController {
 
 
 
-    @PostMapping(value={"","/"})
+    @PostMapping(value={"/jugar"})
     public String guardarConfiguracion(Configuracion configuracion){
         configuracionRepository.save(configuracion);
         return "redirect:/jugar";
     }
+
+    @PostMapping(value={"/minar"})
+    public String guardarMina(@RequestParam("posicionExplosion") String posicion){
+
+        return "redirect:/jugar";
+    }
+
     @GetMapping(value={"/jugar"})
     public String mostrarJuego(Model model){
+
+        Configuracion configuracion = configuracionRepository.getById(1);
+        Integer numFilas = configuracion.getNumFilas();
+        Integer numColumnas = configuracion.getNumColumnas();
+        Integer numIntentos = configuracion.getNumIntentos();
+        Integer numBombas = configuracion.getNumBombas();
+
+        model.addAttribute("numFilas",numFilas);
+        model.addAttribute("numColumnas",numColumnas);
+        model.addAttribute("numIntentos",numIntentos);
+        model.addAttribute("numBombas",numBombas);
 
 
         return "Juego";
